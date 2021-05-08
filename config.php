aef3c6fed25f4846391afe182dacc2ef92bd0193
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-08 21:51:00 +0100

    delete unused files

--- config.php
+++ config.php
@@ -15,43 +15,6 @@ return [
 
     /**
      * --------------------------------------------------------------------------
-     * Enabling the blog generator
-     * --------------------------------------------------------------------------
-     *
-     * When the blog generator is enabled, blade views in "content/_blog" will
-     * be parsed to get blog posts data and generate blog pagination views.
-     *
-     * Disable this option if you don't need  a blog for your website to
-     * get a performance boost.
-     *
-     */
-
-    'enableBlog' => true,
-
-    /**
-     * --------------------------------------------------------------------------
-     * The number of posts in a page
-     * --------------------------------------------------------------------------
-     *
-     * The number of posts to appear on every page in the blog pages.
-     *
-     */
-    'postsPerPage' => 3,
-
-    /**
-     * --------------------------------------------------------------------------
-     * The view used to display blog paginated pages
-     * --------------------------------------------------------------------------
-     *
-     * This blade view will be used to generate the paginated list of
-     * posts for the blog.
-     *
-     */
-
-    'postsListView' => 'blog',
-
-    /**
-     * --------------------------------------------------------------------------
      * Custom variables
      * --------------------------------------------------------------------------
      *
@@ -60,7 +23,7 @@ return [
      *
      */
 
-    'siteName' => 'Katana',
+    'siteName' => 'Legends of Orisha',
 
-    'siteDescription' => 'Yet another awesome site.',
+    'siteDescription' => 'Very thrilling game.',
 ];
