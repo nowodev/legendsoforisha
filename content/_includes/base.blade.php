735251154e6971d27ad87471789ecdd036f937f4
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-08 22:28:32 +0100

    Update base.blade.php

--- content/_includes/base.blade.php
+++ content/_includes/base.blade.php
@@ -1,57 +1,59 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="@yield('pageDescription', $siteDescription)">
-    <title>{{$siteName}} @yield('pageTitle')</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="@url('assets/css/all.css')">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="@yield('pageDescription', $siteDescription)">
+  <title>{{ $siteName }} @yield('pageTitle')</title>
 
-    <link href="@url('assets/css/fonts/etline-font.min.css')" rel="stylesheet">
-    <link href="@url('assets/css/fonts/fontawesome/all.min.css')" rel="stylesheet">
-    <link href="@url('assets/css/fonts/pe-icon-7-stroke.css')" rel="stylesheet">
-    <link href="@url('assets/css/fonts/themify-icons.css')" rel="stylesheet">
+  <!-- CSS -->
+  <link rel="stylesheet" href="@url('assets/css/all.css')">
 
-    <link href="@url('assets/plugins/owl.carousel/owl.carousel.min.css')" rel="stylesheet">
-    
-    <link href="@url('assets/css/main.css')" rel="stylesheet">
-    <link href="@url('assets/css/styles.css')" rel="stylesheet">
-    
-    <!-- Favicons -->
-    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
-    <link rel="icon" href="assets/img/favicon.png">
+  <link href="@url('assets/css/fonts/etline-font.min.css')" rel="stylesheet">
+  <link href="@url('assets/css/fonts/fontawesome/all.min.css')" rel="stylesheet">
+  <link href="@url('assets/css/fonts/pe-icon-7-stroke.css')" rel="stylesheet">
+  <link href="@url('assets/css/fonts/themify-icons.css')" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <!--[if lt IE 9]>
+  <link href="@url('assets/plugins/owl.carousel/owl.carousel.min.css')" rel="stylesheet">
+
+  <link href="@url('assets/css/main.css')" rel="stylesheet">
+  <link href="@url('assets/css/styles.css')" rel="stylesheet">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+  <link rel="icon" href="assets/img/favicon.png">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+  <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
 
 
-  <body class="page-body">
-    
-    @include('_includes.nav')
+<body class="page-body">
+  @include('_includes.nav')
 
-    @yield('body')
+  @yield('body')
 
-      <!-- jQuery -->
-    <script src="@url('assets/js/jquery.min.js')"></script>
+  @include('_includes.footer')
 
-    <!-- Bootstrap -->
-    <script src="@url('assets/js/bootstrap.min.js')"></script>
 
-    <!-- User JS -->
-    <script src="@url('assets/js/scripts.js')"></script>
-
-    <!-- Main JS -->
-    <script src="@url('assets/js/main.js')" id="_mainJS" data-plugins="load"></script>
-  </body>
+  <!-- jQuery -->
+  <script src="@url('assets/js/jquery.min.js')"></script>
+  <!-- Bootstrap -->
+  <script src="@url('assets/js/bootstrap.min.js')"></script>
+  <!-- User JS -->
+  <script src="@url('assets/js/scripts.js')"></script>
+  <!-- Main JS -->
+  <script src="@url('assets/js/main.js')" id="_mainJS" data-plugins="load"></script>
+</body>
 
 </html>
