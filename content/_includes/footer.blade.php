37f04f6fd1314294634b30df825c485ee1999e4e
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-09 20:33:27 +0100

    modify footer

--- content/_includes/footer.blade.php
+++ content/_includes/footer.blade.php
@@ -1,49 +1,49 @@
 <!-- footer -->
 <footer class="footer footer-dark bg-dark py-9">
   <div class="container">
-      <div class="row gutters-y">
-          <div class="col-6 col-lg-3">
-            <a href="#" class="logo d-block mb-4"><img src="assets/img/logo-gaming.png" alt="Wicodus" class="logo-dark"></a>
-            <p>Wicodus is a online store where you can find digital goods at the best prices.</p>
-            <div class="social-buttons">
+    <div class="row gutters-y">
+      <div class="col-6 col-lg-3">
+        <a href="#" class="logo d-block mb-4"><img src="assets/img/logo-gaming.png" alt="Wicodus" class="logo-dark"></a>
+        <p>Dimension 11 is a gaming studio that chokes. E choke die...</p>
+        {{-- <div class="social-buttons">
               <a class="social-twitter" href="#"><i class="fab fa-twitter"></i></a>
               <a class="social-dribbble" href="#"><i class="fab fa-dribbble"></i></a>
               <a class="social-instagram" href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
+            </div> --}}
+      </div>
 
-          <div class="col-6 col-lg-2">
-            <h6 class="text-uppercase fw-600 mb-4">About</h6>
-            <div class="nav flex-column">
-              <a class="nav-link" href="about.html">Our team</a>
-              <a class="nav-link" href="about.html">Careers</a>
-              <a class="nav-link" href="about.html">Cookie Policy</a>
-              <a class="nav-link" href="about.html">Privacy Policy</a>
-              
-            </div>
-          </div>
+      <div class="col-6 col-lg-2">
+        <h6 class="text-uppercase fw-600 mb-4">About</h6>
+        <div class="nav flex-column">
+          <a class="nav-link" href="about.html">Our team</a>
+          <a class="nav-link" href="about.html">Careers</a>
+          {{-- <a class="nav-link" href="about.html">Cookie Policy</a>
+              <a class="nav-link" href="about.html">Privacy Policy</a> --}}
 
-          <div class="col-6 col-lg-2">
-            <h6 class="text-uppercase fw-600 mb-4">Community</h6>
-            <div class="nav flex-column">
-              <a class="nav-link" href="news.html">Forum</a>
-              <a class="nav-link" href="news.html">Blog</a>
-              <a class="nav-link" href="news.html">News</a>
-              <a class="nav-link" href="news.html">Team</a>
-            </div>
-          </div>
+        </div>
+      </div>
 
-          <div class="col-6 col-lg-2">
-            <h6 class="text-uppercase fw-600 mb-4">Help</h6>
-            <div class="nav flex-column">
-              <a class="nav-link" href="contact.html">Contact Us</a>
-              <a class="nav-link" href="contact.html">Support</a>
-              <a class="nav-link" href="contact.html">Terms & conditions</a>
-              <a class="nav-link" href="contact.html">Refund policy</a>
-            </div>
-          </div>
-          
-          <div class="col col-lg-3 order-lg-last">
+      <div class="col-6 col-lg-2">
+        <h6 class="text-uppercase fw-600 mb-4">Community</h6>
+        <div class="nav flex-column">
+          <a class="nav-link" href="news.html">Forum</a>
+          <a class="nav-link" href="news.html">Blog</a>
+          <a class="nav-link" href="news.html">News</a>
+          <a class="nav-link" href="news.html">Team</a>
+        </div>
+      </div>
+
+      <div class="col-6 col-lg-2">
+        <h6 class="text-uppercase fw-600 mb-4">Help</h6>
+        <div class="nav flex-column">
+          <a class="nav-link" href="contact.html">Contact Us</a>
+          <a class="nav-link" href="contact.html">Support</a>
+          <a class="nav-link" href="contact.html">Terms & conditions</a>
+          {{-- <a class="nav-link" href="contact.html">Refund policy</a> --}}
+        </div>
+      </div>
+
+      {{-- <div class="col col-lg-3 order-lg-last">
             <div class="mb-6">
               <h6 class="text-uppercase fw-600 mb-4">Ways to pay</h6>
               <div class="text-light lead-5 lh-1">
@@ -64,8 +64,8 @@
                 <i class="fas fa-star text-secondary"></i>
               </div>
             </div>
-          </div>
-      </div>
+          </div> --}}
+    </div>
   </div>
 </footer>
 <!-- /.footer -->
