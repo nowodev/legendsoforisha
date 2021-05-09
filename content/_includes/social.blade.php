fd29ac3b78229e068ddb3f19600d50e36e536daa
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-09 20:20:18 +0100

    show social links

---
+++ content/_includes/social.blade.php
@@ -0,0 +1,49 @@
+<!-- Contact Us area -->
+<section id="socials" class="content-section latest-articles text-light">
+  <div class="container">
+    <header class="section-header text-center">
+      <h2>Follow Our Progress</h2>
+      <hr class="w-10 border-warning border-top-2 o-90">
+    </header>
+
+    <!-- Social Icons area -->
+    <div class="about-features content-section bp-c br-n bs-c" 
+    {{-- style="background-image: url(assets/img/bg/bg_a.jpg)" --}}
+      data-overlay="8">
+      <div class="container-fluid text-center">
+        <div class="row gutters-y">
+          <div class="col-md-6 col-lg-3 mb-6 mb-lg-0">
+            <div class="display-4 mb-6 instagram">
+              <a href="https://www.instagram.com/dimension11studios/">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-6 mb-lg-0">
+            <div class="display-4 mb-6"">
+              <a href="https://discord.gg/b66VVuU6wt">
+                <i class="fab fa-discord" style="color: #8899f7;></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-6 mb-md-0">
+            <div class="display-4 mb-6">
+              <a href="https://www.youtube.com/channel/UCzgUzhZAUbJk6Czb6cxJ0TA">
+                <i class="fab fa-youtube" style="color: #fc0000"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="display-4 mb-6">
+              <a href="https://www.linkedin.com/company/dimension-11">
+                <i class="fab fa-linkedin" style="color: #0077b5"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.Social Icons area -->
+  </div>
+</section>
+<!-- /.Contact Us area -->
