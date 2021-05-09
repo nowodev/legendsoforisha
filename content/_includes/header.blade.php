0a98c2701f0c154062acdbbffbe7d0b60aba95e7
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-09 01:33:06 +0100

    convert video to background video

--- content/_includes/header.blade.php
+++ content/_includes/header.blade.php
@@ -1,14 +1,16 @@
 <!-- header -->
 <header id="header" class="header h-fullscreen text-light">
-  <div class="media-container parallax-window" data-parallax="scroll" data-image-src="@url('assets/img/content/carousel/slide_01.png')"></div>
+  <div class="media-container parallax-window" data-parallax="scroll"
+    data-image-src="@url('assets/img/content/carousel/slide_01.png')"></div>
   <div class="overlay pe-n bg-dark_A-40"></div>
   <div class="overlay d-flex align-items-center">
     <div class="container text-center">
       <div class="row align-items-center">
         <div class="col-lg-10 mx-auto">
           <div>
-            <video style="width: 100%; height:auto" controls>
-              <source src="assets/videos/promoVid.mp4" type="video/mp4">
+            <video id="myVideo" controls>
+              <source src="assets/videos/promoVid.mp4" poster="assets/img/content/carousel/slide_01.png"
+                type="video/mp4">
               Your browser does not support this video.
             </video>
           </div>
