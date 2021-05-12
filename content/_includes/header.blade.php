31e4adb115ae25443a29aef98ebb5d2eba3602e9
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-12 23:54:28 +0100

    add content to header

--- content/_includes/header.blade.php
+++ content/_includes/header.blade.php
@@ -11,6 +11,20 @@
               Your browser does not support this video.
             </video>
           </div>
+          <section id="about" class="content-section position-relative text-light text-center parallax-window" data-parallax="scroll" data-overlay="">
+            <div class="container position-relative">
+              <div class="row">
+                <div class="col-lg-8 mx-auto">
+                  <div>
+                    <h2 class="ls-1">About</h2>
+                    <hr class="w-10 border-warning border-top-2 o-90">
+                    <p class="lead-1">Quisque sit amet erat posuere scelerisque. Quisque fermentum eget arcu aliquet tempor. Phasellus sit amet bibendum diam, sed vulputate magna. Proin ante dolor, ultricies ac laoreet sed, fermentum sit amet nulla. Suspendisse bibendum dictum tempus. Aliquam turpis mauris, sagittis vitae egestas a, scelerisque. Donec justo turpis, porta a urna vitae, convallis lobortis risus. Integer porta malesuada tristique. Nullam aliquet mattis sollicitudin.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <!-- /.content area -->
         </div>
       </div>
     </div>
