71f7677780c6146bf15a0d69a7cea02e1c9c01c2
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-16 00:40:34 +0100

    adjust social icons grid

--- content/_includes/social.blade.php
+++ content/_includes/social.blade.php
@@ -12,28 +12,28 @@
       data-overlay="8">
       <div class="container-fluid text-center">
         <div class="row gutters-y">
-          <div class="col-md-6 col-lg-3 mb-6 mb-lg-0">
+          <div class="col-sm-6 col-md-6 col-lg-3 mb-6 mb-lg-0">
             <div class="display-4 mb-6">
               <a href="https://www.instagram.com/dimension11studios/" class="text-decoration-none">
                 <i class="fab fa-instagram instagram fa-2x"></i>
               </a>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3 mb-6 mb-md-0">
+          <div class="col-sm-6 col-md-6 col-lg-3 mb-6 mb-md-0">
             <div class="display-4 mb-6">
               <a href="https://www.youtube.com/channel/UCzgUzhZAUbJk6Czb6cxJ0TA">
                 <i class="fab fa-youtube fa-2x" style="color: #fc0000"></i>
               </a>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3 mb-6 mb-lg-0">
+          <div class="col-sm-6 col-md-6 col-lg-3 mb-6 mb-lg-0">
             <div class="display-4 mb-6"">
               <a href="https://discord.gg/b66VVuU6wt">
                 <i class="fab fa-discord fa-2x" style="color: #8899f7;"></i>
               </a>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3">
+          <div class="col-sm-6 col-md-6 col-lg-3">
             <div class="display-4 mb-6">
               <a href="https://www.linkedin.com/company/dimension-11">
                 <i class="fab fa-linkedin fa-2x" style="color: #0077b5"></i>
