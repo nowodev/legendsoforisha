27ed0f4c08fc808c05854f5b6272d701e0227128
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-16 00:16:32 +0100

    build

--- content/index.blade.php
+++ content/index.blade.php
@@ -16,6 +16,10 @@
     <section id="server" class="content-section latest-articles text-light" style="background-image: url(assets/img/bg/bg_shape.png)">
       <div class="about-features content-section bp-c br-n bs-c">
         <div class="container-fluid text-center">
+          <header class="section-header text-center">
+            <h2>Latest Videos</h2>
+            <hr class="w-10 border-warning border-top-2 o-90">
+          </header>
           <div data-mc-src="cd9d51fe-cb16-46ac-b4c1-872767aed16c#youtube" class="mx-auto w-100"></div> 
         </div>
       </div>
