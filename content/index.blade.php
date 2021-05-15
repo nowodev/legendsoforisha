eafc652c8ebd90e1e6939f6562bd590a188bc664
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-15 04:14:43 +0100

    change bg image position

--- content/index.blade.php
+++ content/index.blade.php
@@ -141,8 +141,8 @@
     {{-- DISCORD SERVER SECTION  --}}
 
     {{-- YOUTUBE CAROUSEL  --}}
-    <section id="server" class="content-section latest-articles text-light">
-      <div class="about-features content-section bp-c br-n bs-c" style="background-image: url(assets/img/bg/bg_shape.png)">
+    <section id="server" class="content-section latest-articles text-light" style="background-image: url(assets/img/bg/bg_shape.png)">
+      <div class="about-features content-section bp-c br-n bs-c">
         <div class="container-fluid text-center">
           <div data-mc-src="cd9d51fe-cb16-46ac-b4c1-872767aed16c#youtube" class="mx-auto w-100"></div> 
         </div>
