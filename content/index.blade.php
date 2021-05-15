89f33dbeca1ee00fe869b5a247e84857b40fb850
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-16 00:37:48 +0100

    FORMAT

--- content/index.blade.php
+++ content/index.blade.php
@@ -1,34 +1,35 @@
 @extends('_includes.base')
 
 @section('body')
-  @include('_includes.header')
-
   <main class="main-content">
-    {{-- CHARACTERS SECTION  --}}
-    @include('_includes.characters')
-    {{-- CHARACTERS SECTION  --}}
-    
-    {{-- DISCORD SERVER SECTION  --}}
-    @include('_includes.discord')
-    {{-- DISCORD SERVER SECTION  --}}
+    @include('_includes.header')
 
-    {{-- YOUTUBE CAROUSEL  --}}
-    <section id="server" class="content-section latest-articles text-light" style="background-image: url(assets/img/bg/bg_shape.png)">
+    {{-- CHARACTERS SECTION --}}
+    @include('_includes.characters')
+    {{-- CHARACTERS SECTION --}}
+
+    {{-- DISCORD SERVER SECTION --}}
+    @include('_includes.discord')
+    {{-- DISCORD SERVER SECTION --}}
+
+    {{-- YOUTUBE CAROUSEL --}}
+    <section id="server" class="content-section latest-articles text-light"
+      style="background-image: url(assets/img/bg/bg_shape.png)">
       <div class="about-features content-section bp-c br-n bs-c">
         <div class="container-fluid text-center">
           <header class="section-header text-center">
             <h2>Latest Videos</h2>
             <hr class="w-10 border-warning border-top-2 o-90">
           </header>
-          <div data-mc-src="cd9d51fe-cb16-46ac-b4c1-872767aed16c#youtube" class="mx-auto w-100"></div> 
+          <div data-mc-src="cd9d51fe-cb16-46ac-b4c1-872767aed16c#youtube" class="mx-auto w-100"></div>
         </div>
       </div>
     </section>
-    {{-- YOUTUBE CAROUSEL  --}}
+    {{-- YOUTUBE CAROUSEL --}}
 
-    {{-- FOLLOW OUR PROGRESS  --}}
+    {{-- FOLLOW OUR PROGRESS --}}
     @include('_includes.social')
-    {{-- FOLLOW OUR PROGRESS  --}}
+    {{-- FOLLOW OUR PROGRESS --}}
   </main>
 
 @stop
